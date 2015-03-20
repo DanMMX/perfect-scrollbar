705a9d8343682271b60b89df8222daa7a6f3aae6
Dan Rocha <bairesdev_drocha@iseatz.com>
2015-03-20 11:27:58 -0600

    Changed jQuery to jquery

--- src/perfect-scrollbar.js
+++ src/perfect-scrollbar.js
@@ -7,7 +7,7 @@
 
   if (typeof define === 'function' && define.amd) {
     // AMD. Register as an anonymous module.
-    define(['jQuery'], factory);
+    define(['jquery'], factory);
   } else if (typeof exports === 'object') {
     // Node/CommonJS
     factory(require('jquery'));
